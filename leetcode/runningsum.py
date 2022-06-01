2d5724da3cb14cc22b73840379b4f9933e7d954e
sudhirsinghshekhawat <sudhirsinghshekhawat@gmail.com>
2022-06-01 13:04:03 +0530

    adding the changes for running sum leetcode:1480

--- leetcode/runningsum.py
+++ leetcode/runningsum.py
@@ -2,18 +2,22 @@ from typing import List
 
 
 class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        sum_array = []
-        sum_array.append(nums[0])
-        for i in range(1, len(nums)):
-            sum_array[i] += sum_array[i - 1]
-        return sum_array
+
+    def running_sum(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        for i in range(1, n):
+            nums[i] = nums[i - 1] + nums[i]
+
+        return nums
 
 
-if __name__ == '__main__':
-    # s = Solution()
-    # nums = [1, 2, 3, 4]
-    # result = s.runningSum(nums)
-    # print(result)
-    name = "sudhir singh"
-    print(name.split())
+class TestSolution:
+
+    def setup(self):
+        self.solution = Solution()
+
+    def test_running_sum(self):
+        nums = [1, 2, 3, 4]
+        result = [1, 3, 6, 10]
+        assert result == self.solution.running_sum(nums)
