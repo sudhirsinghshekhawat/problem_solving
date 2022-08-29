97ef551b978be6c80701e276b99d82bd25275ea0
sudhirsinghshekhawat <sudhirsinghshekhawat@gmail.com>
2022-08-29 20:55:54 +0530

    no of island problems leetcode:200

---
+++ leetcode/noofislands.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+    def no_of_islands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid: List[List[str]], i: int, j: int):
+        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
+            return
+
+        grid[i][j] = '#'
+        self.dfs(grid, i + 1, j)
+        self.dfs(grid, i - 1, j)
+        self.dfs(grid, i, j - 1)
+        self.dfs(grid, i, j + 1)
+
+
+if __name__ == '__main__':
+    grid = [
+        ["1", "1", "1", "1", "0"],
+        ["1", "1", "0", "1", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "0", "0", "0"]
+    ]
+
+    solution = Solution()
+    result = solution.no_of_islands(grid)
+    print(result)
